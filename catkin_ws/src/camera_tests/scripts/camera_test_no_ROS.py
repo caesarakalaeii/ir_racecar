bbf440eed631d5a94d8ba159b779a50ca09ba2df
caesarakalaeii <caesarlp@gmx.de>
2023-06-04 12:18:58 +0200

    tests for refactoring, nonfunctional

--- catkin_ws/src/camera_tests/scripts/camera_test_no_ROS.py
+++ catkin_ws/src/camera_tests/scripts/camera_test_no_ROS.py
@@ -7,17 +7,17 @@ import parameters as p
 
 if __name__ == "__main__":
     runtime_list = dict()
-    runtime_list.update({"join_type": 2})
+    runtime_list.update({"join_type": 4})
     runtime_list.update({"verbose":True})
     runtime_list.update({"direct_import": False})
-    runtime_list.update({"static_matrix": True})
+    runtime_list.update({"static_matrix": False})
     runtime_list.update({"timing":False})
     runtime_list.update({"console_log":True})
     for k,v in p.default_list.items():
         if not k in runtime_list.keys():
             runtime_list.update({k:v})
-    cam1 = cv2.VideoCapture(0)
-    cam2 = cv2.VideoCapture(2)
+    cam1 = cv2.VideoCapture(2)
+    cam2 = cv2.VideoCapture(0)
     joiner = join.ImageJoinFactory.create_instance(runtime_list)
     
     while True:
@@ -34,11 +34,15 @@ if __name__ == "__main__":
             #frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
             b, frame2 = cam2.read()
             if a == False or b == False:
-                raise Exception("Couldnt open Cameras")
+                continue
+                #raise Exception("Couldnt open Cameras")
             #frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
             cv2.imshow("Cam1", frame1)
             cv2.imshow("Cam2", frame2)
-            cv2.imshow("Joined", joiner.blending(frame1, frame2))
+            try:
+                cv2.imshow("Joined", joiner.blending(frame1, frame2))
+            except:
+                continue
         except:
             
             traceback.print_exc()
