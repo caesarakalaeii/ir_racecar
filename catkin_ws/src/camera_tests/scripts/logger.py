70b998f6583b7494b039ff7bb7e387185f279321
ngoam76 <nabchau76@gmail.com>
2023-06-05 16:54:59 +0200

    Changing 'print' to 'logger'

--- catkin_ws/src/camera_tests/scripts/logger.py
+++ catkin_ws/src/camera_tests/scripts/logger.py
@@ -14,35 +14,35 @@ class Logger():
         
         self.console_log = console_log
 
-    def warning(self, skk):
+    def warning(self, skk): #yellow
         
         if self.console_log:
             print("\033[93m {}\033[00m" .format("WARNING:"),"\033[93m {}\033[00m" .format(skk))
         if self.ros_log:
             rospy.logwarn(skk)
        
-    def error(self, skk):
+    def error(self, skk): #red
         if self.console_log:   
             print("\033[91m {}\033[00m" .format("ERROR:"),"\033[91m {}\033[00m" .format(skk))
         if self.ros_log:
             rospy.logerr(skk)
         
-    def fail(self, skk):
+    def fail(self, skk): #red
         if self.console_log: 
             print("\033[91m {}\033[00m" .format("FATAL:"),"\033[91m {}\033[00m" .format(skk))
         if self.ros_log:
             rospy.logfatal(skk)
-    def passing(self, skk): 
+    def passing(self, skk): #green
         if self.console_log: 
             print("\033[92m {}\033[00m" .format(skk))
         if self.ros_log:
             rospy.loginfo(skk)
-    def passingblue(self, skk): 
+    def passingblue(self, skk): #blue
         if self.console_log: 
             print("\033[96m {}\033[00m" .format(skk))
         if self.ros_log:
             rospy.loginfo(skk)
-    def info(self, skk): 
+    def info(self, skk): #blue
         if self.console_log: 
             print("\033[94m {}\033[00m" .format("Info:"),"\033[94m {}\033[00m" .format(skk))
         if self.ros_log:
