2766f25c260761aea496845ba4db495ebea19fdc
mdannehl <markus.dannehl@stud.hs-bochum.de>
2023-08-02 14:53:28 +0200

    fixing

--- catkin_ws/src/camera_tests/scripts/image_join_cuda.py
+++ catkin_ws/src/camera_tests/scripts/image_join_cuda.py
@@ -101,11 +101,11 @@ class ImageJoinCuda(ImageJoin):
             mask[:, barrier + offset:] = 1
             time_masking = time.time() 
         if not hasDepth:
-            r = cv.cuda.merge([cv.cuda.GpuMat(mask)]).download()
+            r = cv.cuda.merge([mask])
             self.logger.info(f"Time to prepare: {preparation-start_time}\nTime to Mask: {time_masking-preparation}\nTime to merge{time.time()-time_masking}")
             return r
 
-        r = cv.cuda.merge([cv.cuda.GpuMat(mask), cv.cuda.GpuMat(mask), cv.cuda.GpuMat(mask)]).download
+        r = cv.cuda.merge([mask,mask,mask])
         #self.logger.info(f"Time to prepare: {preparation-start_time}\nTime to Mask: {time_masking-preparation}\nTime to merge{time.time()-time_masking}")
         return r
 
@@ -149,7 +149,7 @@ class ImageJoinCuda(ImageJoin):
                 self.mask1 =  mask1
             pano = time.time()
             panorama1[0:img1.shape[0], 0:img1.shape[1]] = img1
-            panorama1 = cv.cuda.multiply(panorama1,mask1).download() #evtl durch primitive ersetzen (a*b)
+            panorama1 = cv.cuda.multiply(panorama1,mask1) #evtl durch primitive ersetzen (a*b)
             self.logger.info(f"Time for masking panorama1: {time.time()-pano}")
             expected_time += time.time()-pano
             if self.static_mask and self.mask_set:
@@ -169,7 +169,7 @@ class ImageJoinCuda(ImageJoin):
                 convertUMat = time.time()
                 warped = cv.cuda.warpPerspective(src, M, dsize = (width_panorama, height_panorama)).download()
                 warponGPU = time.time()
-                panorama2 = warped * mask2
+                panorama2 = cv.cuda.multiply(warped,mask2)
                 end = time.time()
                 self.logger.info(f"Time to transform to GPUMat: {convertGPU-start}\nTime to transform to UMat: {convertUMat-convertGPU}\nTime to warp on GPU: {warponGPU-convertUMat}\nTotal elapsed time: {end-start}\n")
                 expected_time += convertGPU-start
@@ -178,7 +178,7 @@ class ImageJoinCuda(ImageJoin):
             except:
                 raise Exception("Couldn't match images.")
             start = time.time()
-            result=panorama1+panorama2 #evtl durch primitive ersetzen (a+b)
+            result=cv.cuda.add(panorama1,panorama2) #evtl durch primitive ersetzen (a+b)
             end = time.time()
             self.logger.info(f"Time to add images on CPU: {end-start}")
             expected_time += end-start
