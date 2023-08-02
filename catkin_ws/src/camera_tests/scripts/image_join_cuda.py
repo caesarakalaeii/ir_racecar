3895c737b24c1eea57353a8ded35382c4eaf3ef1
mdannehl <markus.dannehl@stud.hs-bochum.de>
2023-08-02 12:11:12 +0200

    fixed wrong logging

--- catkin_ws/src/camera_tests/scripts/image_join_cuda.py
+++ catkin_ws/src/camera_tests/scripts/image_join_cuda.py
@@ -145,7 +145,7 @@ class ImageJoinCuda(ImageJoin):
                 warponGPU = time.time()
                 panorama2 = warped * mask2
                 end = time.time()
-                self.logger.info(f"Time to transform to GPUMat: {convertGPU-start}\nTime to transform to UMat: {convertUMat-convertGPU}\nTime to warp on GPU: {convertUMat-warponGPU}\nTotal elapsed time: {end-start}\n")
+                self.logger.info(f"Time to transform to GPUMat: {convertGPU-start}\nTime to transform to UMat: {convertGPU-convertUMat}\nTime to warp on GPU: {convertUMat-warponGPU}\nTotal elapsed time: {end-start}\n")
             except:
                 raise Exception("Couldn't match images.")
             start = time.time()
