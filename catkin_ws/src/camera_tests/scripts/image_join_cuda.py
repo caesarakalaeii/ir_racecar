2b5783b8ecba327aeb810ed90aae14409aa59bfa
mdannehl <markus.dannehl@stud.hs-bochum.de>
2023-08-02 15:54:25 +0200

    more testing

--- catkin_ws/src/camera_tests/scripts/image_join_cuda.py
+++ catkin_ws/src/camera_tests/scripts/image_join_cuda.py
@@ -57,9 +57,9 @@ class ImageJoinCuda(ImageJoin):
         
 
     def registration(self,img1,img2):
-        img1_UMat = cv.UMat(img1)
+        img1_UMat = cv.cuda.GpuMat(img1)
         #img1_UMat.upload(img1)
-        img2_UMat = cv.UMat(img2)
+        img2_UMat = cv.cuda.GpuMat(img2)
         self.logger.info(f"{type(img1_UMat)}")
         #img2_UMat.upload(img2)
         kp1 = self.finder.detect(img1_UMat)
