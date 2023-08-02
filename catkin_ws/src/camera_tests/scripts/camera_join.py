bd5a32bc40e002722e5f254e14292aefcf153ea2
mdannehl <markus.dannehl@stud.hs-bochum.de>
2023-08-02 15:01:46 +0200

    trying to debug

--- catkin_ws/src/camera_tests/scripts/camera_join.py
+++ catkin_ws/src/camera_tests/scripts/camera_join.py
@@ -180,33 +180,33 @@ class CameraJoin(object):
                 if self.timing:
                     start_blending = t.time()
                 old_image = self.image_joined
-                try:
-                    self.image_joined = self.stitcher.blending(self.image1, self.image2)
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    # time when we finish processing for this frame
-                    new_frame_time = time.time()
+                #try:
+                self.image_joined = self.stitcher.blending(self.image1, self.image2)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                # time when we finish processing for this frame
+                new_frame_time = time.time()
 
-                    # Calculating the fps
+                # Calculating the fps
 
-                    # fps will be number of frame processed in given time frame
-                    # since their will be most of time error of 0.001 second
-                    # we will be subtracting it to get more accurate result
-                    fps = 1/(new_frame_time-self.prev_frame_time)
-                    self.prev_frame_time = new_frame_time
+                # fps will be number of frame processed in given time frame
+                # since their will be most of time error of 0.001 second
+                # we will be subtracting it to get more accurate result
+                fps = 1/(new_frame_time-self.prev_frame_time)
+                self.prev_frame_time = new_frame_time
 
-                    # converting the fps into integer
-                    fps = int(fps)
+                # converting the fps into integer
+                fps = int(fps)
 
-                    # converting the fps to string so that we can display it on frame
-                    # by using putText function
-                    fps = str(fps)
+                # converting the fps to string so that we can display it on frame
+                # by using putText function
+                fps = str(fps)
 
-                    # putting the FPS count on the frame
-                    cv2.putText(self.image_joined, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
-                except Exception as e:
-                    if self.VERBOSE:
-                        self.l.warning("Stale image will be used: {}".format(e))
-                    self.image_joined = old_image
+                # putting the FPS count on the frame
+                cv2.putText(self.image_joined, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+                #except Exception as e:
+                #    if self.VERBOSE:
+                #        self.l.warning("Stale image will be used: {}".format(e))
+                #    self.image_joined = old_image
                 if self.timing:
                     end_blending = t.time()
                     self.l.info(f"Time to blend: {(end_blending-start_blending)*1000}ms")
